d01c550f6ffc978a4b3729d52a4cc04a7025bd37
Javier Perez <javier21perez98@gmail.com>
2021-04-29 13:15:49 -0700

    troubleshooted error with app and tested, now is full working and able to seed

--- server.js
+++ server.js
@@ -1,20 +1,20 @@
-require("dotenv").config();
-const express = require("express");
-const sequelize = require("./config/connection");
-const router = require("./controllers");
-const exphbs = require("express-handlebars");
-const helpers = require("./util/helpers");
-const path = require("path");
-const sessionMiddleware = require("./config/session");
+require('dotenv').config();
+const express = require('express');
+const sequelize = require('./config/connection');
+const router = require('./controllers');
+const exphbs = require('express-handlebars');
+const helpers = require('./util/helpers');
+const path = require('path');
+const sessionMiddleware = require('./config/session');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 // setup app middleware
 app.use(sessionMiddleware);
-app.use(express.static(path.join(__dirname, "public")));
-app.engine("handlebars", exphbs({ helpers }));
-app.set("view engine", "handlebars");
+app.use(express.static(path.join(__dirname, 'public')));
+app.engine('handlebars', exphbs({ helpers }));
+app.set('view engine', 'handlebars');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -23,17 +23,17 @@ app.use(router);
 
 // connect db and listen
 sequelize
-    .sync({ force: false })
-    .then(() => {
-        app.listen(PORT, (err) => {
-            if (err) {
-                console.error(err);
-                return process.exit(1);
-            }
-            console.log(`App listening on PORT ${PORT}`);
-        });
-    })
-    .catch((err) => {
+  .sync({ force: false })
+  .then(() => {
+    app.listen(PORT, (err) => {
+      if (err) {
         console.error(err);
-        process.exit(1);
+        return process.exit(1);
+      }
+      console.log(`App listening on PORT ${PORT}`);
     });
+  })
+  .catch((err) => {
+    console.error(err);
+    process.exit(1);
+  });
