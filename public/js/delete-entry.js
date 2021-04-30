2d3f812c7b6368600c252712d4058d508f313ce2
Javier Perez <javier21perez98@gmail.com>
2021-04-30 16:29:40 -0700

    function added to select clicked button and get data id

--- public/js/delete-entry.js
+++ public/js/delete-entry.js
@@ -1,3 +1,10 @@
+const budgetEntryHandler = (event) => {
+  //check if current trgt is delete button
+  if (event.target.closest('.delete-button')) {
+    const id = event.target.closest('.delete-button').getAttribute('data-id');
+    deleteEntry(id);
+  }
+};
 document
   .querySelector('.budget-entries')
   .addEventListener('click', budgetEntryHandler);
