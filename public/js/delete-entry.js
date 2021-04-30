16c8f66e52c0a4af1e2599b9c22f1aacef705351
Javier Perez <javier21perez98@gmail.com>
2021-04-30 16:30:36 -0700

    added function to send fetch request to delete linked to row

--- public/js/delete-entry.js
+++ public/js/delete-entry.js
@@ -1,3 +1,18 @@
+const deleteEntry = async (id) => {
+  // create url
+  const url = `/api/expense/${id}`;
+  // send fetch request
+  const response = await fetch(url, {
+    method: 'DELETE',
+  });
+  // if good req
+  if (response.ok) {
+    // reload page
+    document.location.replace('/dashboard');
+  } else {
+    alert('Failed to delete project');
+  }
+};
 const budgetEntryHandler = (event) => {
   //check if current trgt is delete button
   if (event.target.closest('.delete-button')) {
