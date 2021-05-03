e8000be6381683c1956a3d7fd64a2683fee85161
Entropy <79678575+hdezbriant@users.noreply.github.com>
2021-05-02 19:31:14 -0700

    Budget amount now updates

--- public/js/edit-entry.js
+++ public/js/edit-entry.js
@@ -1,14 +1,14 @@
 const editExpenseHandler = async (event) => {
   event.preventDefault();
 
-  const planned_amount = document.querySelector('#plannedAmount').value.trim();
+  const budget_amount = document.querySelector('#plannedAmount').value.trim();
   const id = document.querySelector('#plannedAmount');
   const amount_spent = document.querySelector('#actualAmount').value.trim();
 
-  if (planned_amount && amount_spent) {
+  if (budget_amount && amount_spent) {
     const response = await fetch(`/api/expense/` + id.dataset.id, {
       method: 'PUT',
-      body: JSON.stringify({ planned_amount , amount_spent }),
+      body: JSON.stringify({ budget_amount , amount_spent }),
       headers: {
         'Content-Type': 'application/json',
       },
